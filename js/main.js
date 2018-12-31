79666c1ecb9920f0c5c469ad1d4a93bb9e34666b
johnny1864 <johnny1864@yahoo.com>
2018-12-30 16:43:02 -0800

    added book, UI, and storage classes

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// BOOK CLASS 
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI CLASS
+
+class UI {
+    static displayBooks() {
+        let books = [
+            {
+                title: 'book one',
+                author: 'Author one',
+                isbn: '12345'
+           },
+            {
+                title: 'book one',
+                author: 'Author one',
+                isbn: '12345'
+           }
+       ];
+    }
+}
+
+// STORAGE CLASS
+
+class Storage {
+    
+}
+
+
+// DISPLAY BOOKS
+
+// ADD BOOK
+
+// REMOVE BOOK
